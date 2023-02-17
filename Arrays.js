44398472d5d138db861fdd5d402ad1c3b388eeb6
Prathamesh Chavan <114760937+Web3eer@users.noreply.github.com>
2023-02-17 13:02:38 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,7 @@
+const words=["Hello","World","of","Arrays"];
+console.log("You can print full array like"+" "+words);
+console.log("Also can be extracted one by one like");
+console.log(words[0]);
+console.log(words[1]);
+console.log(words[2]);
+console.log(words[3]);
